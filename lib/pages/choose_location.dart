071cc4fe526c26a9a1233eb1de52062dfd6f4a3b
DIYOUH PETER <102492516+Petasco@users.noreply.github.com>
2023-11-28 23:46:46 +0000

    Update choose_location.dart

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,10 +11,10 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
 
   List<WorldTime> locations = [
-    WorldTime(location: 'Ghana - Accra', flag: 'ghana-flag.jpg', url: 'Africa/Ghana', isDaytime: true),
+    WorldTime(location: 'Ghana', flag: 'ghana-flag.jpg', url: 'Africa/Ghana', isDaytime: true),
     WorldTime(location: 'London', flag: 'uk-flag.jpg', url: 'Europe/London', isDaytime: true),
     WorldTime(location: 'America', flag: 'argentina-flag.jpg', url: 'Argentina/Salta', isDaytime: true),
-    WorldTime(location: 'Seoul', flag: 'south-korea-flag.jpg', url: 'Asia/Seoul', isDaytime: true),
+    WorldTime(location: 'South Korea - Seoul', flag: 'south-korea-flag.jpg', url: 'Asia/Seoul', isDaytime: true),
     WorldTime(location: 'Berlin', flag: 'germany-flag.jpg', url: 'Europe/Berlin', isDaytime: true),
     WorldTime(location: 'New York', flag: 'uk-flag.jpg', url: 'America/New_York', isDaytime: true),
     WorldTime(location: 'Nigeria - Legos', flag: 'nigeria-flag.jpg', url: 'Africa/Nigeria', isDaytime: true),
@@ -23,24 +23,34 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(location: 'Ghana - Accra', flag: 'ghana-flag.jpg', url: 'Africa/Ghana', isDaytime: true),
     WorldTime(location: 'London', flag: 'uk-flag.jpg', url: 'Europe/London', isDaytime: true),
     WorldTime(location: 'America', flag: 'argentina-flag.jpg', url: 'Argentina/Salta', isDaytime: true),
-    WorldTime(location: 'Seoul', flag: 'south-korea-flag.jpg', url: 'Asia/Seoul', isDaytime: true),
-    WorldTime(location: 'Berlin', flag: 'germany-flag.jpg', url: 'Europe/Berlin', isDaytime: true),
+    WorldTime(location: 'South-Korea - Seoul', flag: 'south-korea-flag.jpg', url: 'Asia/Seoul', isDaytime: true),
+    WorldTime(location: 'Germany - Berlin', flag: 'germany-flag.jpg', url: 'Europe/Berlin', isDaytime: true),
     WorldTime(location: 'New York', flag: 'uk-flag.jpg', url: 'America/New_York', isDaytime: true),
     WorldTime(location: 'Nigeria - Legos', flag: 'nigeria-flag.jpg', url: 'Africa/Nigeria', isDaytime: true),
     WorldTime(location: 'Cairo', flag: 'egypt-flag.gif', url: 'Africa/Egypt', isDaytime: true),
   ];
 
-  void updateTime(index) async {
-    WorldTime instance = locations[index];
-    await instance.getTime();
 
-    Navigator.pop(context, {
-      'locations': instance.location,
-      'flag': instance.flag,
-      'time': instance.time,
-      'isDaytime': instance.isDaytime,
-    });
+
+  void updateTime(index) async {
+    WorldTime? instance = locations[index];
+    print('Instance: $instance');
+
+    if (instance != null) {
+      await instance.getTime();
+      print('Time after getTime: ${instance.time}');
+
+      Navigator.pop(context, {
+        'locations': instance.location ?? '',
+        'flag': instance.flag ?? '',
+        'time': instance.time?.toString() ?? '', // Use toString() to handle nullability
+        'isDaytime': instance.isDaytime ?? false,
+      });
+    }
   }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -72,4 +82,28 @@ class _ChooseLocationState extends State<ChooseLocation> {
         }),
     );
   }
+
+
 }
+
+
+/*
+  void updateTime(index) async {
+   try{
+     WorldTime? instance = locations[index]; // Add the "?" to make it nullable
+
+     if (instance != null) {
+       await instance.getTime();
+
+       Navigator.pop(context, {
+         'locations': instance.location ?? '', // Use the null-aware operator here
+         'flag': instance.flag ?? '',         // Use the null-aware operator here
+         'time': instance.time ?? '',         // Use the null-aware operator here
+         'isDaytime': instance.isDaytime ?? false, // Use the null-aware operator here
+       });
+     }
+   }
+   catch (e){
+     print("Oops: $e");
+   }
+  } */
